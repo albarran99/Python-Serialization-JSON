22280e7a88518372eb71de4713ae541684f0eacd
ralbarran1 <ralbarran@colway-08.com>
2022-07-29 13:34:47 +0200

    change a variable name

--- Serialization.py
+++ Serialization.py
@@ -5,23 +5,23 @@ from Person import say_hello, Humans
 from Non_organic import say_hello_nonOrganic, Bionicle
 from Animal import say_hello_animals, Monsters
 
-user1 = Humans('Saytama', 22, 'Japan', 'O+')
-user2 = Bionicle('Vezon', 16, 'Voya nui', 'teletransporte', 20, 4)
-user3 = Monsters('Rathalos', 17, 'Japan', 'llamas', 20, 4)
-user4 = Monsters('Zinogre', 12, 'Japan', 'truenos', 20, 4)
-user5 = Bionicle('Vezok', 16, 'Japan', 'ladron de poder', 20, 4)
+character_1 = Humans('Saytama', 22, 'Japan', 'O+')
+character_2 = Bionicle('Vezon', 16, 'Voya nui', 'teletransporte', 20, 4)
+character_3 = Monsters('Rathalos', 17, 'Japan', 'llamas', 20, 4)
+character_4 = Monsters('Zinogre', 12, 'Japan', 'truenos', 20, 4)
+character_5 = Bionicle('Vezok', 16, 'Japan', 'ladron de poder', 20, 4)
 
 ram_command = random.randint(1, 2)
 
-say_hello(user1)
+say_hello(character_1)
 print("\n")
-say_hello_nonOrganic(user2)
+say_hello_nonOrganic(character_2)
 print("\n")
-say_hello_animals(user3)
+say_hello_animals(character_3)
 print("\n")
-say_hello_animals(user4)
+say_hello_animals(character_4)
 print("\n")
-say_hello_nonOrganic(user5)
+say_hello_nonOrganic(character_5)
 print("\n")
 
 
@@ -49,15 +49,15 @@ def mini_figth():
     while user_command != 'q':
         if user_command == 'hit':
             if user_choose_character == 'vezon':
-                print(user_choose_character + ' va a atacar a ' + user3.name + '\n')
+                print(user_choose_character + ' va a atacar a ' + character_3.name + '\n')
 
-                user3.pv = user3.pv - user2.dmg
+                character_3.pv = character_3.pv - character_2.dmg
 
-                if user3.pv == 0:
-                    print(user3.name + ' Ha sido derrotado' + f'(tu vida actual: {user2.pv}) \n')
+                if character_3.pv == 0:
+                    print(character_3.name + ' Ha sido derrotado' + f'(tu vida actual: {character_2.pv}) \n')
                     quit(0)
 
-                print('Uff!, eso dolió ahora ' + user3.name + ' le queda de vida ' + str(user3.pv) + f'(tu vida actual: {user2.pv})')
+                print('Uff!, eso dolió ahora ' + character_3.name + ' le queda de vida ' + str(character_3.pv) + f'(tu vida actual: {character_2.pv})')
 
                 enemy_ia()
 
@@ -73,16 +73,16 @@ def enemy_ia():
     if ram_command == 1:
         print('Oh no! va a atacar ')
 
-        user2.pv = user2.pv - user3.dmg
+        character_2.pv = character_2.pv - character_3.dmg
 
-        if user2.pv == 0:
-            print(user2.name + ' Has perdido')
+        if character_2.pv == 0:
+            print(character_2.name + ' Has perdido')
             quit(0)
 
-        print(f'(tu vida actual: {user2.pv}) \n')
+        print(f'(tu vida actual: {character_2.pv}) \n')
 
     elif ram_command == 2:
-        print('genial!, a fallado el ataque' + f'(tu vida actual: {user2.pv})')
+        print('genial!, a fallado el ataque' + f'(tu vida actual: {character_2.pv})')
 
 
 mini_figth()
