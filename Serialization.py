3efbd766b562ccdd075f15b29b3d763470d90b53
ralbarran1 <ralbarran@colway-08.com>
2022-07-29 10:01:27 +0200

    creaete a pojo'User'

--- Serialization.py
+++ Serialization.py
@@ -1,17 +1,22 @@
 import json
+from User import user_to_dict, user_from_dict
 
 data = {
-    "user": {
-        "name": "Saytama (One Punch Man)",
-        "age": 22,
-        "place": "Japan",
-        "Blood group": "0+"
-    }
+    "name": "Saytama (One Punch Man)",
+    "age": 22,
+    "place": "Japan",
+    "blood_group": "0+"
 }
+
+usr = user_from_dict(data)
+print(usr.name)
+
+usr_dict = user_to_dict(usr)
+print(usr_dict["place"])
 
 # with open("datafile.json" , "w") as write:
 #     json.dump(data , write)
 
-user = json.dumps(data)
-
-print(user)
+# user = json.dumps(data)
+#
+# print(user)
